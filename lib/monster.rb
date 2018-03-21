4b3c29ad5d030e0fd58b062032b3bc0096c5d012
Antonia Cronin <antoniajcronin@gmail.com>
2018-03-21 13:33:38 +0000

    play method

--- lib/monster.rb
+++ lib/monster.rb
@@ -1,33 +1,31 @@
 # understands how to run the monster game
 class Monster
-  attr_reader :choices
+  attr_reader :choices, :answer
 
   def initialize
+    @opening_message = p 'When a Monster is Born ... there are two possibilities - '
     @choices = [
-      { choice1: 'it\'s a FARAWAY-IN-THE-FORESTS monster',
-        choice2: 'it\'s an UNDER-YOUR-BED monster' },
-      { choice1: 'it EATS YOU',
-        choice2: 'you make friends and TAKE IT TO SCHOOL.' },
-      { choice1: 'it SITS QUIETLY, does its HOMEWORK and becomes the first monster to play for the SCHOOL BASKETBALL TEAM',
-        choice2: 'it eats the HEAD TEACHER.' },
-      { choice1: 'it growls "YUMMY!" and DANCES BOOGIE-WOOGIE',
-        choice2: 'it growls "SORRY!" and WALKS OFF through the wall.' },
-      { choice1: 'it SITS IN THE PARK and scratches its head',
-        choice2: 'it takes a deep breath and sets off for the FARAWAY-FORESTS' },
-      { choice1: 'it finds an EXPENSIVE HOTEL on the way, and decides to sleep in it',
-        choice2: 'it goes round the back of the hotel, finds a BROKEN UMBRELLA and decides to sleep under that' },
-      { choice1: 'a kitchen girl comes out and tips a SAUCEPAN OF PORRIDGE over the monster\'s head',
-        choice2: 'the kitchen girl notices the monster and STOPS IN HER TRACKS' },
-      { choice1: 'the monster gives her the fright of her life. "GRRROARRRR!", and she runs off shouting, "HELP! HELP! HELP!"',
-        choice2: 'the monster gives her a rose and they FALL IN LOVE' },
-      { choice1: 'she kisses the monster and it turns into a HANDSOME YOUNG MAN',
-        choice2: 'it kisses her and she turns into a MONSTER' },
-      { choice1: 'the monster says "UUUUUUUUURGH! You look horrible now!"',
-        choice2: 'the monster says "Look, I\'m a monster, you\'re a monster. Let\'s get MARRIED"' },
-      { choice1: 'the two of them live happily together and have a BABY MONSTER',
-        choice2: 'they EAT EACH OTHER' },
-      { choice1: 'it\'s a FARAWAY-IN-THE-FORESTS monster',
-        choice2: 'it\'s an UNDER-YOUR-BED monster...' }
+      { a: 'it\'s a FARAWAY-IN-THE-FORESTS monster',
+        b: 'it\'s an UNDER-YOUR-BED monster' },
+      { a: 'it EATS YOU',
+        b: 'you make friends and TAKE IT TO SCHOOL.' }
     ]
   end
+
+  def play
+    @answer = 'a'
+    @opening_message
+
+    while @answer == 'a'
+      @choices.map do |choice|
+        p "either a/ #{choice[:a]} OR b/ #{choice[:b]} <br /> Make your choice: a or b"
+        @answer = gets.chomp
+        if @answer == 'b'
+          return "That's that!"
+        end
+      end
+      return "That's that!"
+    end
+  end
+
 end
